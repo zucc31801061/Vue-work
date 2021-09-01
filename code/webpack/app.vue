d9c5297f2cc763b296b7efd4ced54f48dc63ec15
zucc31801061 <617109717@qq.com>
2021-09-01 19:25:58 +0800

    upload

---
+++ code/webpack/app.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<v-title title="Vue 组件化"></v-title>
+		<v-button @click="handleClick">点击按钮</v-button>
+		<p>
+			<img src="./images/2.jpg" style="width: 200px;"/>
+		</p>
+	</div>
+</template>
+<script>
+	import vTitle from './title.vue';
+	import vButton from './button.vue';
+	export default{
+		components:{
+			vTitle,
+			vButton
+		},
+		methods:{
+			handleClick(e){
+				console.log(e);
+			}
+		}	
+	}
+</script>
